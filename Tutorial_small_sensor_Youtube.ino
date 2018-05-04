9233f9a1baa98482da19e05b08373b64c4d530c2
Sparklenymph <36805484+Sparklenymph@users.noreply.github.com>
2018-05-05 01:07:21 +1000

    Refined Capacitive Touch Sensor
    As opposed to the Original. This is a modified version based on the 20min Youtube tutorial. The original is in case anything weird happens to this one.

--- Tutorial_small_sensor_Youtube.ino
+++ Tutorial_small_sensor_Youtube.ino
@@ -4,6 +4,8 @@ CapacitiveSensor cs1 = CapacitiveSensor(6,7);
 CapacitiveSensor cs2 = CapacitiveSensor(6,8);
 CapacitiveSensor cs3 = CapacitiveSensor(6,9);
 
+boolean cs1g = true;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -17,39 +19,58 @@ void loop() {
    long cs2v = cs2.capacitiveSensor(30);
     long cs3v = cs3.capacitiveSensor(30);
 
+Serial.println(cs1v);
+//  Serial.println(" ");
+
+  if (cs1g){
+    if (cs1v > 20){
+      Serial.println(cs1v);
+      digitalWrite (13, HIGH);
+      cs1g = false;
+  }
+  }
+
+  if(!cs1g){
+    if(cs1v < 20){
+      Serial.println(cs1v);
+      digitalWrite(13, LOW);
+      cs1g = true;
+    }
+  }
+
   Serial.println(cs1v);
-  Serial.println(" ");
-
   
-if (cs1v > 200)
-  { 
-    digitalWrite (13, HIGH);
-  }
-  else
-  {
-    digitalWrite(13, LOW);
-  }
-
+delay(5);
   
-if (cs2v > 200)
-  {
-    digitalWrite(13, HIGH);
-  }
-  else
-  {
-    digitalWrite(13, LOW);
-  }
-Serial.println(cs2v);
-  
-if (cs3v > 200)
-  {
-    digitalWrite(13, HIGH);
-  }
-  else
-  {
-    digitalWrite(13, LOW);
-  }
-
-  Serial.println(cs3v);
-   delay(15);
+//if (cs1v > 200)
+//  { 
+//    digitalWrite (13, HIGH);
+//  }
+//  else
+//  {
+//    digitalWrite(13, LOW);
+//  }
+//
+//  
+//if (cs2v > 200)
+//  {
+//    digitalWrite(13, HIGH);
+//  }
+//  else
+//  {
+//    digitalWrite(13, LOW);
+//  }
+//Serial.println(cs1v);
+//  
+//if (cs3v > 200)
+//  {
+//    digitalWrite(13, HIGH);
+//  }
+//  else
+//  {
+//    digitalWrite(13, LOW);
+//  }
+//
+//  Serial.println(cs2v);
+//   delay(100);
 }
